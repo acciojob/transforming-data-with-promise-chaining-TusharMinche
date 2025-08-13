68fe5566ccc6188313b14f9e9318de5e001ed049
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-13 18:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-//your JS code here. If required.
-
-let btn =  document.getElementById("btn");
+let btn = document.getElementById("btn");
 let output = document.getElementById("output");
 
 btn.addEventListener("click", () => {
-	let mypromise = new Promise((res, rej) => {
-		setTimeout(() => {
-			// alert("Hi");
-			let input = document.getElementById("ip").value;
-			
-			res(input);
-		}, 2000);
-	})
-		
-	mypromise.then((data) => {
-		output.innerHTML = `Result: ${data}`;
-		return new Promise((res, rej) => {
-			setTimeout(() => {
-				res(data*2);
-			}, 2000)
-		})
-	}).then((data) => {
-		output.innerHTML = `Result: ${data}`;
-		return new Promise((res, rej) => {
-			setTimeout(() => {
-				res(data-3);
-			}, 1000)
-		})
-	}).then((data) => {
-		output.innerHTML = `Result: ${data}`;
-		return new Promise((res, rej) => {
-			setTimeout(() => {
-				res(data/2);
-			}, 1000)
-		})
-	}).then((data) => {
-		output.innerHTML = `Result: ${data}`;
-		return new Promise((res, rej) => {
-			setTimeout(() => {
-				res(data+10);
-			}, 1000)
-		})
-	}).then((data) => {
-		output.innerHTML = `Result: ${data}`;
-	})
-})
+    let mypromise = new Promise((res) => {
+        setTimeout(() => {
+            let input = document.getElementById("ip").value;
+            res(Number(input));
+        }, 2000); // First step: 2 seconds
+    });
+
+    mypromise
+        .then((data) => {
+            output.innerHTML = `Result: ${data}`;
+            return new Promise((res) => {
+                setTimeout(() => {
+                    res(data * 2);
+                }, 1000); // Cypress expects 1 sec here
+            });
+        })
+        .then((data) => {
+            output.innerHTML = `Result: ${data}`;
+            return new Promise((res) => {
+                setTimeout(() => {
+                    res(data - 3);
+                }, 1000);
+            });
+        })
+        .then((data) => {
+            output.innerHTML = `Result: ${data}`;
+            return new Promise((res) => {
+                setTimeout(() => {
+                    res(data / 2);
+                }, 1000);
+            });
+        })
+        .then((data) => {
+            output.innerHTML = `Result: ${data}`;
+            return new Promise((res) => {
+                setTimeout(() => {
+                    res(data + 10);
+                }, 1000);
+            });
+        })
+        .then((data) => {
+            output.innerHTML = `Final Result: ${data}`; // Match test expectation
+        });
+});
